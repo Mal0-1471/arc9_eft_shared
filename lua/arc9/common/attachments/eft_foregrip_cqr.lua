f2d1fcaa55db8154602b2d5d7ba2131cb851e4ec
Darsu <95997959+dar-su@users.noreply.github.com>
2023-02-19 00:25:38 +0700

    ww

--- lua/arc9/common/attachments/eft_foregrip_cqr.lua
+++ lua/arc9/common/attachments/eft_foregrip_cqr.lua
@@ -3,7 +3,7 @@ ATT.CompactName = "CQR"
 ATT.Description = [[The CQR front grip is an easy to install, lightweight, and compact frontgrip, manufactured by Hera Arms. In combination with the CQR Buttstock, operators will get a lightweight and ergonomic SBR system.]]
 ATT.Icon = Material("entities/eft_attachments/foregrips/cqr.png", "mips smooth")
 
-ATT.Model = "models/weapons/arc9/darsu_eft/mods/fg_cqr.mdl"
+ATT.Model = "models/weapons/arc9/darsu_eft/mods/fg_heracqr.mdl"
 
 ATT.LHIK_Priority = 2
 ATT.LHIK = true
@@ -14,5 +14,5 @@ ATT.RecoilMult = 0.97
 ATT.VisualRecoilMult = 0.97
 
 ATT.SortOrder = 0
-ATT.Category = "eft_foregrip_big"
+ATT.Category = "eft_foregrip_large"
 ATT.MenuCategory = "ARC9 - EFT Attachments"
