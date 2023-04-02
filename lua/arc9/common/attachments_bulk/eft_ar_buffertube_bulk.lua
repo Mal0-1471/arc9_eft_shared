fcf7da9d5c4f9289ae68673c1c2844ac1d3d392b
Darsu <95997959+dar-su@users.noreply.github.com>
2023-04-02 23:35:33 +0700

    fixed viper stock

--- lua/arc9/common/attachments_bulk/eft_ar_buffertube_bulk.lua
+++ lua/arc9/common/attachments_bulk/eft_ar_buffertube_bulk.lua
@@ -422,7 +422,7 @@ ATT.CompactName = "Viper PDW"
 ATT.Description = [[The Strike Industries Viper PDW stock kit is a retractable stock installed on AR-15 weapon systems. Reduces the overall weapon size and recoil buffer.]]
 ATT.Icon = Material("entities/eft_attachments/stocks/viperpdw.png", "mips smooth")
 
-ATT.Model = "models/weapons/arc9/darsu_eft/mods/stock_ar15_troy_m7a1_pdw.mdl"
+ATT.Model = "models/weapons/arc9/darsu_eft/mods/stock_ar15_strike_industries_viper_pdw_stock.mdl"
 
 ATT.HasBufferTube = true
 
