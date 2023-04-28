efedb28449c54f297495bbf9733e0a127db40118
Darsu <95997959+dar-su@users.noreply.github.com>
2023-04-28 14:19:58 +0700

    localiz

---
+++ lua/arc9/common/localization/eft_uwu.lua
@@ -0,0 +1,89 @@
+L = {}
+
+-- [[ Trivia ]]
+
+L["eft_trivia_author"] = "Authowow :3"
+L["eft_trivia_assets"] = "Awwets"
+L["eft_trivia_help"] = "Genewaw hewp"
+L["eft_trivia_arc9"] = "AWC9 Bawe dumb! >w<"
+
+L["eft_trivia_manuf"] = "Manuwactuwew owo"
+L["eft_trivia_cal"] = "Cawibww"
+L["eft_trivia_act"] = "Actiwon"
+L["eft_trivia_country"] = "Countwy uwu"
+L["eft_trivia_year"] = "Yeaww"
+
+L["eft_trivia_manuf_kalash"] = "Kalawnikoww Concewn"
+L["eft_trivia_manuf_molot"] = "Mowot Owwuzhie"
+L["eft_trivia_manuf_tula"] = "Tuwa Awms Pwant"
+L["eft_trivia_manuf_tskib"] = "TsKIB SOWO"
+L["eft_trivia_manuf_kbp"] = "KBP Instwument Dewign Buweauwu"
+
+L["eft_trivia_act_bolt"] = "Bowt >w<"
+L["eft_trivia_act_blow"] = "Bwow uwu"
+L["eft_trivia_act_gas"] = "Gaws owo"
+
+L["eft_trivia_country_ussr"] = "Sowiet Uniown uwu"
+L["eft_trivia_country_rus"] = "Rusian Fedewation owo"
+L["eft_trivia_country_belg"] = "Kingw-dom of Bewgium"
+L["eft_trivia_country_brit"] = "Uniwed Kinwgdom"
+L["eft_trivia_country_usa"] = "USwA 'w'"
+L["eft_trivia_country_usa_eu"] = "USwA/Euwuwope"
+L["eft_trivia_country_aust"] = "Wepublwic of Austwia"
+L["eft_trivia_country_germ"] = "Gewmany"
+
+-- [[ Weapon Categories ]]
+L["eft_subcat_carb"] = "Awwault Cawwbines owo"
+L["eft_subcat_ar"] = "Awwault Wiffles uwu"
+L["eft_subcat_sg"] = "Showtguwuns"
+L["eft_subcat_snip"] = "Snipew/Mawksmwan Wifles"
+L["eft_subcat_smg"] = "Suwubmawwine Guwns"
+L["eft_subcat_pist"] = "Pistows"
+
+-- [[ Weapon Classes ]]
+L["eft_class_weapon_singleshot"] = "Single-shot Wifle"
+L["eft_class_weapon_boltsg"] = "Bowt-actiowon Showtgun"
+L["eft_class_weapon_pist"] = "Pistow"
+L["eft_class_weapon_revol"] = "Rowowwer"
+L["eft_class_weapon_megapist"] = "Machwine Pistow"
+L["eft_class_weapon_smg"] = "Suwubmawwine Guwn"
+L["eft_class_weapon_ar"] = "Awwault Wifle"
+L["eft_class_weapon_carb"] = "Cawwbine Wifle"
+L["eft_class_weapon_semisg"] = "Semi-autowo Showtgun"
+L["eft_class_weapon_pumpsg"] = "Puwump Showtgun"
+L["eft_class_weapon_lmg"] = "Liwwt Machwine Guwn"
+L["eft_class_weapon_boltsnip"] = "Bowt-actiowon Sniper Wifle"
+L["eft_class_weapon_marks"] = "Mawksmwan Wifle"
+
+-- [[ Weapon Display Names ]]
+L["eft_weapon_rsh12"] = "WShh-12"
+L["eft_weapon_ash12"] = "AShh-12"
+L["eft_weapon_toz106"] = "TOWww-106"
+L["eft_weapon_ak74"] = "AK-74"
+L["eft_weapon_ak74n"] = "AK-74N"
+L["eft_weapon_ak74m"] = "AK-74Mwa"
+L["eft_weapon_aks74"] = "AKS-74"
+L["eft_weapon_aks74n"] = "AKS-74N"
+L["eft_weapon_aks74u"] = "AKS-74Uwu"
+L["eft_weapon_aks74un"] = "AKS-74UwuN"
+L["eft_weapon_aks74ub"] = "AKS-74UBoo"
+L["eft_weapon_ak101"] = "AK-101"
+L["eft_weapon_ak102"] = "AK-102"
+L["eft_weapon_ak103"] = "AK-103"
+L["eft_weapon_ak104"] = "AK-104"
+L["eft_weapon_ak105"] = "AK-105"
+L["eft_weapon_akm"] = "AKMwa"
+L["eft_weapon_akmn"] = "AKMN"
+L["eft_weapon_akms"] = "AKMSs"
+L["eft_weapon_akmsn"] = "AKMSN"
+L["eft_weapon_pp1901"] = "PP-19-01 \"OWOWityaw\""
+L["eft_weapon_rpk16"] = "WPK-16"
+L["eft_weapon_saiga9"] = "Saigawa-9"
+L["eft_weapon_saiga12k"] = "Saigawa 12ga wew.10"
+L["eft_weapon_vpo136"] = "WPO-136 \"UWUpr-KM\""
+L["eft_weapon_vpo209"] = "WPO-209"
+
+
+-- [[ Weapon descs ]] fuckkkkkkkk
+-- L["eft_weapon_vpo209"] = "VPO-209"
+
