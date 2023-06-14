cc205a857d22b0402d31cc88b80dee3237f71122
Darsu <95997959+dar-su@users.noreply.github.com>
2023-06-14 17:12:24 +0700

    a

--- lua/arc9/common/sh_eft_xplosions.lua
+++ lua/arc9/common/sh_eft_xplosions.lua
@@ -41,6 +41,13 @@ end
 local fhcolortab = {
 	[ "$pp_colour_brightness" ] = 0,
 	[ "$pp_colour_contrast" ] = 1,
+	[ "$pp_colour_addr" ] = 0,
+	[ "$pp_colour_addg" ] = 0,
+	[ "$pp_colour_addb" ] = 0,
+	[ "$pp_colour_colour" ] = 1,
+	[ "$pp_colour_mulr" ] = 0,
+	[ "$pp_colour_mulg" ] = 0,
+	[ "$pp_colour_mulb" ] = 0
 }
 
 local function EFTIsLookingAt(targetVec)
