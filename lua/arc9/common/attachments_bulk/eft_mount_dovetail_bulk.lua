1806e1556cf26b19d3b394e2f145b724cdc6d1f7
Darsu <95997959+dar-su@users.noreply.github.com>
2024-05-03 01:50:30 +0700

    sag thing and old old mats updated

--- lua/arc9/common/attachments_bulk/eft_mount_dovetail_bulk.lua
+++ lua/arc9/common/attachments_bulk/eft_mount_dovetail_bulk.lua
@@ -269,3 +269,40 @@ ATT.ModelAngleOffset = Angle(0, 90, 0)
 
 ARC9.LoadAttachment(ATT, "eft_mount_dovetail_sagbit")
 
+
+
+///////////////////////////////////////      eft_mount_dovetail_sag
+
+
+ATT = {}
+
+ATT.PrintName = "SAG AK dovetail sidemount"
+ATT.CompactName = "SAG AK"
+ATT.Icon = Material("entities/eft_attachments/mount/dt_sag.png", "mips smooth")
+ATT.Description = [[The "SAG AK" sidemount is designed to mount sights on AK rifles and their numerous versions: Saiga, SVD, etc. Manufactured by Sureshot Armament Group.]]
+
+ATT.Model = "models/weapons/arc9_eft_shared/atts/mounts/mount_dovetail_sag_ak.mdl"
+
+ATT.Category = {"eft_mount_dovetail"}
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+
+ATT.ModelOffset = Vector(0, 0, 0)
+
+ATT.EFTErgoAdd = -1
+ATT.CustomCons = { Ergonomics = "-1" }
+ATT.ActivateElements = {"nolongrear"}
+
+ATT.Attachments = {
+    {
+        PrintName = "Optic",
+        Category = {"eft_optic_medium", "eft_optic_large", "eft_optic_small"},
+        Pos = Vector(1, 1.05, -2.2),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(0, 1, 0),
+        -- ExtraSightDistance = 8
+    },
+}
+
+ATT.ModelAngleOffset = Angle(0, 90, 0)
+
+ARC9.LoadAttachment(ATT, "eft_mount_dovetail_sag")
