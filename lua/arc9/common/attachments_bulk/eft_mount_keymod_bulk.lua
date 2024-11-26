d9527c03a3ae82c95fde4662b45c339b30b4fec0
Darsu <95997959+dar-su@users.noreply.github.com>
2024-11-26 23:16:57 +0700

    new rails

--- lua/arc9/common/attachments_bulk/eft_mount_keymod_bulk.lua
+++ lua/arc9/common/attachments_bulk/eft_mount_keymod_bulk.lua
@@ -145,7 +145,7 @@ ATT.Description = [[Vltor CASV guide for KeyMod 2 inch system allows you to inst
 ATT.Model = "models/weapons/arc9_eft_shared/atts/mounts/keymod.mdl"
 ATT.ModelBodygroups = "2"
 
-ATT.Category = {"eft_mount_keymod", "eft_mount_keymod2"}
+ATT.Category = {"eft_mount_keymod", "eft_mount_keymod2", "eft_mount_keymod_casv2"}
 ATT.MenuCategory = "ARC9 - EFT Attachments"
 
 ATT.ModelOffset = Vector(0, 0, 0)
@@ -257,3 +257,64 @@ ATT.Attachments = {
 }
 
 ARC9.LoadAttachment(ATT, "eft_mount_keymod_bipodd")
+
+///////////////////////////////////////      eft_mount_keymod_corvus18
+
+
+ATT = {}
+
+ATT.PrintName = "Corvus Defensio KeyMod 1.8 inch rail section"
+ATT.CompactName = "CD 1.8\""
+ATT.Icon = Material("entities/eft_attachments/mount/casvkm2.png", "mips smooth")
+ATT.Description = [[The Corvus Defensio 1.8 inch KeyMod rail allows installation of additional equipment on the handguards equipped with a standard KeyMod interface.]]
+
+ATT.Model = "models/weapons/arc9_eft_shared/atts/mounts/keymod_corvus.mdl"
+-- ATT.ModelBodygroups = "0"
+
+ATT.Category = {"eft_mount_keymod", "eft_mount_keymod2", "eft_mount_keymod2_corvusonly"}
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+
+ATT.ModelOffset = Vector(0, 0, 0)
+
+ATT.Attachments = {
+    {
+        PrintName = "Tactical",
+        Category = "eft_tactical",
+        RejectAttachments = { ["eft_tactical_raptar"] = true },
+        Pos = Vector(-0.2, -0.61, 0),
+        Ang = Angle(0, 0, -90),
+        Icon_Offset = Vector(0, 0, 0),
+    },
+}
+
+
+ARC9.LoadAttachment(ATT, "eft_mount_keymod_corvus18")
+
+///////////////////////////////////////      eft_mount_keymod_corvus68
+
+ATT = {}
+
+ATT.PrintName = "Corvus Defensio KeyMod 6.8 inch rail section"
+ATT.CompactName = "CD 6.8\""
+ATT.Icon = Material("entities/eft_attachments/mount/casvkm6.png", "mips smooth")
+ATT.Description = [[The Corvus Defensio 6.8 inch KeyMod rail allows installation of additional equipment on the handguards equipped with a standard KeyMod interface.]]
+
+ATT.Model = "models/weapons/arc9_eft_shared/atts/mounts/keymod_corvus.mdl"
+ATT.ModelBodygroups = "1"
+
+ATT.Category = {"eft_mount_keymod6"}
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+
+ATT.ModelOffset = Vector(0, 0, 0)
+
+ATT.Attachments = {
+    {
+        PrintName = "Grip",
+        Category = {"eft_foregrip_small","eft_foregrip_large"},
+        Pos = Vector(0.75, -0.39, 0),
+        Ang = Angle(0, 0, 90),
+        Icon_Offset = Vector(0, 0, -1),
+    },
+}
+
+ARC9.LoadAttachment(ATT, "eft_mount_keymod_corvus68")
