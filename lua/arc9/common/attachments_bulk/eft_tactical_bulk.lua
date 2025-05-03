ac5c70355fdb201dc370d44361e0f7c3758ec774
Darsu <95997959+dar-su@users.noreply.github.com>
2025-05-03 18:48:26 +0700

    eft_shared_fastreload

--- lua/arc9/common/attachments_bulk/eft_tactical_bulk.lua
+++ lua/arc9/common/attachments_bulk/eft_tactical_bulk.lua
@@ -1931,3 +1931,37 @@ ATT.Attachments = {
 
 
 ARC9.LoadAttachment(ATT, "eft_mount_kac_bipod")
+
+
+
+
+///////////////////////////////////////      eft_shared_fastreload
+
+
+ATT = {}
+
+ATT.PrintName = "Dump cylinder on every reload"
+ATT.CompactName = "Fast reload"
+ATT.Icon = Material("entities/eft_ash12_attachments/fastreload.png", "mips smooth")
+ATT.Description = [[Attachment instead of double pressing R in EFT.
+Drops all ammo on ground on any reload for faster reload.]]
+
+ATT.EFTForceFastReload = true
+
+ATT.AttNotForNPCs = true 
+ATT.Max = 1
+
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+
+ATT.Category = {"eft_custom_slot_revolver"}
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "eft_shared_fastreload")
